9f645949238e88c94c61ab9515f573b1b4cb9f50
Yashodeep928 <yashodeepdeshpande84639@gmail.com>
2026-08-24 21:55:20 +0530

    Working on the players logic

---
+++ index.js
@@ -0,0 +1,28 @@
+const gamezone = document.getElementById("gameArea")
+// const box = document.createElement("div")
+
+// box.style.background = "red"
+// box.style.width = "50px"
+// box.style.height = "50px"
+// box.style.position = "absolute"
+
+
+for(let i =0; i<3; i++){
+    const box = document.createElement("div")
+
+box.style.background = "red"
+box.style.width = "50px"
+box.style.height = "50px"
+box.style.position = "absolute"
+    const randomX = Math.random() * window.innerWidth;
+
+const randomY = Math.random() * window.innerHeight;
+
+box.style.left = randomX + "px"
+box.style.top = randomY + "px"
+
+document.body.appendChild(box)
+
+}
+
+
